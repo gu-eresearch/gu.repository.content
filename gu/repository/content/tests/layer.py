ac6c51176327ef5d9c3ab8f0bd9ba981db1e94e4
Gerhard Weis <gerhard.weis@gmail.com>
2012-11-06 10:56:35 +1000

    added simple tests

---
+++ gu/repository/content/tests/layer.py
@@ -0,0 +1,37 @@
+from plone.testing import z2
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting, FunctionalTesting
+from plone.app.testing import applyProfile
+#from plone.app.testing improt quickInstallProduct
+#quickInstallProduct(portal, productname, reinstall=False)
+
+
+class RepositoryContentFixture(PloneSandboxLayer):
+    """Configure gu.repository.content"""
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+
+        import gu.repository.content
+        self.loadZCML(package=gu.repository.content)
+
+    def setUpPloneSite(self, portal):
+        """ do special site setup here"""
+        applyProfile(portal, 'gu.repository.content:default')
+
+    def tearDownZope(self, app):
+        z2.uninstallProduct(app, 'gu.repository.content')
+
+REPOSITORYCONTENT_FIXTURE = RepositoryContentFixture()
+
+REPOSITORYCONTENT_INTEGRAL_TESTING = IntegrationTesting(bases=(REPOSITORYCONTENT_FIXTURE, ), name="RepositoryContentFixture:Integral")
+REPOSITORYCONTENT_FUNCTIONAL_TESTING = FunctionalTesting(bases=(REPOSITORYCONTENT_FIXTURE, ), name="RepositoryContentFixture:Functional")
+REPOSITORYCONTENT_ZSERVER = FunctionalTesting(bases=(REPOSITORYCONTENT_FIXTURE, z2.ZSERVER_FIXTURE), name="RepositoryContentFixture:ZServer")
+
+
+# Notes: use PloneSandboxLayer and setupZope, setupPloneSite (+ tearDownXXX), to create a custom Plone site fixture
+
+
+#         zcml.load_config("configure.zcml", gu.repository.content)
---
+++ gu/repository/content/tests/test_setup.py
@@ -0,0 +1,50 @@
+#import unittest2 as unittest
+import unittest
+
+from plone.app.testing import TEST_USER_NAME, TEST_USER_ID
+from plone.app.testing import login, setRoles
+#from plone.app.testing import logout
+
+from gu.repository.content.tests.layer import REPOSITORYCONTENT_INTEGRAL_TESTING
+from Products.CMFCore.utils import getToolByName
+
+
+class SetupTest(unittest.TestCase):
+
+    layer = REPOSITORYCONTENT_INTEGRAL_TESTING
+
+    def test_quickinstalls(self):
+        portal = self.layer['portal']
+
+        quickinstaller = getToolByName(portal, 'portal_quickinstaller')
+        self.assertTrue(quickinstaller.isProductInstalled('gu.repository.content'))
+
+    def test_ftis(self):
+        portal = self.layer['portal']
+
+        typesTool = getToolByName(portal, 'portal_types')
+        self.assertNotEqual(typesTool.getTypeInfo('gu.repository.content.RepositoryContainer'), None)
+        self.assertNotEqual(typesTool.getTypeInfo('gu.repository.content.RepositoryItem'), None)
+
+        # TODO: get rid of stuff below
+        # alternative way to do the above?
+        login(portal, TEST_USER_NAME)
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        self.assertTrue('gu.repository.content.RepositoryContainer' in portal.portal_types)
+
+    def test_roles(self):
+        portal = self.layer['portal']
+        # rolemap.xml
+        # verify new role has been added
+        self.assertTrue('Repository Administrator' in portal.valid_roles())
+        # verify permissions have been assigned
+        roles = [r['name'] for r in portal.rolesOfPermission('gu.repository.content: Add Repository Container') if r['selected']]
+        self.assertEqual(roles, ['Manager', 'Repository Administrator'])
+        roles = [r['name'] for r in portal.rolesOfPermission('gu.repository.content: Add Repository Item') if r['selected']]
+        self.assertEqual(roles, ['Manager', 'Repository Administrator'])
+
+    # def test_registry(self):
+    #     portal = self.layer['portal']
+    #     jsRegistry = getToolByName(portal, 'portal_javascripts')
+
+    #     self.assertTrue('myscript.js' in jsRegistry.getResourceIds())
