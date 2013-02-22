a8868d3655856baacb91d7ea4fed9542823ca547
Gerhard Weis <gerhard.weis@gmail.com>
2013-02-22 16:53:22 +1000

    added searchable text indexer for repository content

--- gu/repository/content/repositorycontainer.py
+++ gu/repository/content/repositorycontainer.py
@@ -1,9 +1,19 @@
 from zope.interface import implements
 from plone.dexterity.content import Container
 from gu.repository.content.interfaces import IRepositoryContainer
+from plone.indexer.decorator import indexer
 
 class RepositoryContainer(Container):
 
     implements(IRepositoryContainer)
 
     body = None
+
+
+@indexer(IRepositoryContainer)
+def repositoryContainer_SearchableText(object, **kw):
+    return ' '.join(
+        (object.title,
+         object.description,
+         object.body.output,
+         ))
--- gu/repository/content/repositoryitem.py
+++ gu/repository/content/repositoryitem.py
@@ -1,6 +1,7 @@
 from zope.interface import implements
 from plone.dexterity.content import Container
 from gu.repository.content.interfaces import IRepositoryItem
+from plone.indexer.decorator import indexer
 
 
 class RepositoryItem(Container):
@@ -8,3 +9,11 @@ class RepositoryItem(Container):
     implements(IRepositoryItem)
 
     body = None
+
+@indexer(IRepositoryItem)
+def repositoryItem_SearchableText(object, **kw):
+    return ' '.join(
+        (object.title,
+         object.description,
+         object.body.output,
+         ))
